73ef005a3e8780a1cedb5dfe296c62701b154474
HS H <3262398@naver.com>
2023-05-17 17:36:15 +0900

    0517 리액트 미들웨어

--- react-toolkit/src/App.js
+++ react-toolkit/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import CounterBox from './components/CounterBox'
+import CounterBox from './components/CounterBox';
 import MemoComp from './components/MemoComp';
+import ThunkBox from './components/ThunkBox';
+import WeatherComp from './components/WeatherComp';
 
 function App() {
   return (
     <div className="App">
       <CounterBox />
       <MemoComp />
+      <ThunkBox />
+      <WeatherComp />
     </div>
   );
 }
